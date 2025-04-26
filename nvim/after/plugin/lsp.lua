9422c55fccb146855d04c8ad68422c7199e3b10a
alexk49 <alexcode9@mailbox.org>
2025-04-26 09:57:09 +0100

    Removed pylsp from lsp config and added ruff to ale config.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,43 +8,6 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
-lspconfig.pylsp.setup{
-    on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-        -- Mappings.
-        local opts = { noremap=true, silent=true }
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-        vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
-    end,
-    settings = {
-        pylsp = {
-            plugins = {
-                jedi = {
-                    enabled = true
-                },
-                pycodestyle = {
-                    ignore = {"E501"}
-                }
-            }
-        }
-    }
-}
-
 -- (Optional) Configure lua language server for neovim
 -- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
--- vim/plugin/ale.vim
+++ vim/plugin/ale.vim
@@ -6,11 +6,16 @@ let g:ale_enabled = 0
 " toggle ALE shortcut
 nnoremap <leader>l :ALEToggle<Return>
 
+" Run ALE Fixers
+nnoremap <leader>f :ALEFix<Return>
+
 " use global installations of linters
-let b:ale_python_flake8_use_global = 1
+"let b:ale_python_flake8_use_global = 1
 let b:ale_python_mypy_use_global = 1
-let b:ale_python_black_use_global = 1
-let b:ale_python_autoflake_use_global = 1
+"let b:ale_python_black_use_global = 1
+"let b:ale_python_autoflake_use_global = 1
+let b:ale_python_ruff_use_global = 1
+let b:ale_python_ruff_format_use_global = 1
 
 " stop ALE messages showing as comments
 let g:ale_virtualtext_cursor = 0
@@ -18,9 +23,9 @@ let g:ale_virtualtext_cursor = 0
 " set virtual environment names
 let g:ale_virtualenv_dir_names = ['venv', '.venv', 'env']
 
-let g:ale_python_flake8_options = '--config=~/.config/flake8'
+"let g:ale_python_flake8_options = '--config=~/.config/flake8'
 
-let g:ale_python_black_options = '--config=~/.black'
+"let g:ale_python_black_options = '--config=~/.black'
 
 let g:ale_python_mypy_options = '--ignore-missing-imports'
 
@@ -28,7 +33,7 @@ let g:ale_python_mypy_options = '--ignore-missing-imports'
 let g:ale_linters_explicit = 1
 
 let g:ale_linters = {
-            \ 'python': ['ruff', 'flake8', 'mypy'],
+            \ 'python': ['ruff', 'mypy'],
             \ 'javascript': ['standard', 'eslint'],
             \ 'html': ['tidy'],
             \ 'css': ['stylelint'],
@@ -39,14 +44,17 @@ let g:ale_linters = {
             \ 'text': ['proselint'],
             \ }
 
+let g:ale_python_ruff_format_options = '--unfixable=F401 --line-length=120'
+
+
 " set ALE fixers
 let g:ale_fixers = {
 \   '*': ['remove_trailing_lines', 'trim_whitespace'],
 \   'javascript': ['prettier', 'standard'],
-\   'python': ['black', 'autoflake', 'isort'],
+\   'python':  ['ruff', 'ruff_format'],
 \   'html': ['prettier'],
 \   'css': ['prettier'],
 \}
 
 " fix files when you save them.
-let g:ale_fix_on_save = 1
+" let g:ale_fix_on_save = 1
